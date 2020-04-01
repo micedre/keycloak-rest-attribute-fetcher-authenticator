58725012c1b85ef570c797332443645eef919719
Cédric Couralet <cedric.couralet@insee.fr>
2020-04-01 11:21:23 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rest Attribute Fetcher authenticator for keycloak
+
+**:warning: Work in progress**
+
+
+* Create a jar with `mvn package`
+* Copy the jar to `$KEYCLOAK_HOME/standalone/deployments`
+
---
+++ src/main/java/fr/insee/keycloak/authentication/RestAttributeFetcherFactory.java
@@ -0,0 +1,94 @@
+package fr.insee.keycloak.authentication;
+
+import java.util.List;
+
+import org.jboss.logging.Logger;
+import org.keycloak.Config.Scope;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel.Requirement;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+/**
+ * HashicorpVaultProviderFactory
+ */
+public class RestAttributeFetcherFactory implements AuthenticatorFactory {
+   private static final Logger logger = Logger.getLogger(RestAttributeFetcherFactory.class);
+
+   @Override
+   public Authenticator create(KeycloakSession session) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void init(Scope config) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void postInit(KeycloakSessionFactory factory) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void close() {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public String getId() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getDisplayType() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getReferenceCategory() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean isConfigurable() {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public Requirement[] getRequirementChoices() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean isUserSetupAllowed() {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public String getHelpText() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public List<ProviderConfigProperty> getConfigProperties() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+    
+   
+}
